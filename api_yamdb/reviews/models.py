37dae1a0a6edb1517b3a993cb95e4c37f34c10d3
Ляйсан Галиева <risatip@yandex.ru>
2022-08-18 20:19:43 +0500

    добавлены 3 модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -44,3 +44,61 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Category(models.Model):
+    """Модель для определения категории."""
+    name = models.CharField(max_length=50, verbose_name='Название категории')
+    slug = models.SlugField(unique=True, verbose_name='slug')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    """Модель для определения жанра."""
+    name = models.CharField(max_length=50, verbose_name='Название жанра')
+    slug = models.SlugField(unique=True, verbose_name='slug')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    """Модель для определения произведений."""
+    name = models.CharField(max_length=50, verbose_name='Название произведения')
+    year = models.PositiveSmallIntegerField(
+        db_index=True, validators=[MaxValueValidator(date.today().year)],
+        verbose_name='Год создания произведения'
+    )
+    description = models.TextField(
+        blank=True, max_length=200,
+        verbose_name='Описание'
+    )
+    genre = models.ManyToManyField(
+        Genre, related_name='titles',
+        blank=True, verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name='titles', blank=True,
+        null=True, verbose_name='Категория'
+    )
+
+    class Meta:
+        ordering = ('year',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name[:15]
