0d70dbb86c10bddc2cf29daf0b84f112be82284b
Ляйсан Галиева <risatip@yandex.ru>
2022-08-21 17:51:58 +0500

    добавлены сериализатор,вьюха, юрл

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,1 +1,37 @@
 from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор категории."""
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+    def create(self, validated_data):
+        category = Category.objects.create(**validated_data)
+        return category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор жанра."""
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+    def create(self, validated_data):
+        genre = Genre.objects.create(**validated_data)
+        return genre
+
+
+class TitleSlugSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(many=True, slug_field='slug',
+                                         queryset=Genre.objects.all())
+    category = serializers.SlugRelatedField(slug_field='slug',
+                                            queryset=Category.objects.all())
+
+    class Meta:
+        model = Title
+        fields = '__all__'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+class CategoryViewSet(mixins.ListModelMixin,
+                      mixins.CreateModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet, ):
+    """Класс представления категории."""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [&&&]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class GenreViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericViewSet, ):
+    """Класс представления жанра."""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [&&&]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name',)
+    lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Класс представления произведения."""
+    queryset = Title.objects.annotate(rating=Avg('reviews__score'))
+    permission_classes = (&&&)
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    filter_backends = (OrderingFilter, DjangoFilterBackend)
+    ordering = ('name',)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleSerializer
+        return TitleCreateSerializer
