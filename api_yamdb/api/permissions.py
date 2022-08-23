dbd408e3fa1b7d3ea22e1de8e67140163583ab8e
Дима Шелепин <oladushkin777@gmail.com>
2022-08-23 15:08:17 +0300

    Поправлен пермишен апи и requirements

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,27 +3,29 @@ from rest_framework import permissions
 
 class IsAdminUser(permissions.BasePermission):
 
-    def has_permission(self, request):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        return bool(
-            request.user and request.user.is_admin
-            or request.user and request.user.is_superuser
-        )
+        else:
+            if request.user.is_authenticated:
+                return bool( 
+                    request.user.is_admin is True
+                    or request.user.is_superuser is True
+                )
 
 
 class IsAuthenticated(permissions.BasePermission):
 
-    def has_permission(self, request):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return bool(
-            request.user and request.user.is_active
-            or request.user and request.user.is_admin
-            or request.user and request.user.is_superuser
-        )
+        else:
+            if request.user.is_authenticated:
+                return bool( 
+                    request.user.is_admin is True
+                    or request.user.is_superuser is True
+                )
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
@@ -31,6 +33,6 @@ class IsAuthenticated(permissions.BasePermission):
 
         return bool(
             obj.owner and request.user.is_authenticated
-            or request.user and request.user.is_admin
-            or request.user and request.user.is_superuser
+            or request.user.is_admin is True
+            or request.user.is_superuser is True
         )
